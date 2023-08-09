c8131c3f2dbcbccb05f5c02a7af7e3efb414f5b5
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-08-09 23:47:53 +0500

    Решение 14.3.3. Практика (HW-03).

--- Program.cs
+++ Program.cs
@@ -1,10 +1,43 @@
 ﻿namespace PhoneBook
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //создаем телефонную книгу с контактами
+            List<Contact> phoneBook = new()
+            {
+                new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"),
+                new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"),
+                new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"),
+                new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"),
+                new Contact("Сергей", "Брин", 799900000013, "serg@example.com"),
+                new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com")
+            };
+
+            while (true)
+            {
+                Console.WriteLine("Введите номер страницы от 1 до 3");
+                var input = Console.ReadKey().KeyChar;
+                var parsed = Int32.TryParse(input.ToString(), out int pageNumber);
+                if (!parsed || pageNumber < 1 || pageNumber > 3)
+                {
+                    Console.WriteLine("Страницы не существует");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    var pageContent = phoneBook.OrderBy(contact => contact.Name)
+                        .ThenBy(contact => contact.LastName)
+                        .Skip((pageNumber - 1) * 2)
+                        .Take(2);
+                    Console.WriteLine();
+                    foreach (var entry in pageContent)
+                        Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber);
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
